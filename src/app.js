1e3272fa908b595c3176bdd2ad53db8fd8755d80
Shiva <shiv@shiv.me>
2016-08-15 22:21:54 -0700

    Upgrade to ES6

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
-var styles = require('./styles/main.styl')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styles from './styles/main.styl'
 
-var Hello = React.createClass({
-  displayName: 'Hello',
-  render: function () {
-    return <div>Hello, {this.props.name}</div>
-  }
+const Hello = React.createClass({
+    displayName: 'Hello',
+    render() {
+        return <div>Hello, {this.props.name}</div>
+    }
 })
 
 ReactDOM.render(<Hello name='World!'/>, document.body)
