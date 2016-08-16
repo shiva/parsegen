af68cf5bdc538cb135beaaebf327ee4f8dfb46b4
Shiva <shiv@shiv.me>
2016-08-15 22:15:20 -0700

    fixes to support latest react + functioning app using ES5

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var styles = require('./styles/main.styl')
 
-console.log('Hello, world!')
+var Hello = React.createClass({
+  displayName: 'Hello',
+  render: function () {
+    return <div>Hello, {this.props.name}</div>
+  }
+})
+
+ReactDOM.render(<Hello name='World!'/>, document.body)
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,5 @@ var getConfig = require('hjs-webpack')
 module.exports = getConfig({
     in: 'src/app.js',
     out: 'public',
-    isDev: process.env.NODE_ENV !== 'production'
+    isDev: process.env.NODE_ENV !== 'production',
 })
